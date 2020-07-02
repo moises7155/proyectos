50204c7722f59d123709b7e50ac0f164024f7be4
unknown <l.gamm960817@itses.edu.mx>
2020-07-01 21:07:40 -0500

    componente_registrar

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import {AuthGuard} from "./shared";
 
 const routes: Routes = [
   {
-    path:'',
-    loadChildren:() => import('./layout/layout.module').then((m) => m.LayoutModule),
+    path: '',
+    loadChildren: () => import('./layout/layout.module').then((m) => m.LayoutModule),
     canActivate: [AuthGuard]
   },
   { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', loadChildren: () => import('./login/login.module').then((m) => m.LoginModule) },
+  { path: 'registrar', loadChildren: () => import('./registrar/registrar.module').then((m) => m.RegistrarModule) },
   { path: 'dashboard', loadChildren: () => import('./layout/dashboard/dashboard.module').then((m) => m.DashboardModule) }
 ];
 
